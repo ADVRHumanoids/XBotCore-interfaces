a0c45939d426b4475459800fc99df529732aa48c
Arturo Laurenzi <arturo.laurenzi@iit.it>
2017-03-07 17:48:44 +0100

    Tentative ipc with message queues not working

--- include/XBotCore-interfaces/XBotPipes.h
+++ include/XBotCore-interfaces/XBotPipes.h
@@ -29,6 +29,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <mqueue.h>
 #endif
 
 #ifdef __XENO__
@@ -52,7 +53,7 @@ namespace XBot{
      * @brief XDDP (Cross Domain Datagram Protocol) pipes: useful to communicate beetween N-RT and RT threads.
      *        It can be used both in Xenomai and standard linux kernel: XDDP in the former case, FIFO in the latter.
      *        Check https://xenomai.org/documentation/trunk/html/api/xddp-echo_8c-example.html for more info.
-     * 
+     *
      */
     class XDDP_pipe {
 
@@ -61,21 +62,21 @@ namespace XBot{
         /**
          * @brief XDDP pipe constructor
          *
-         * @param _pool_size local pool size: memory needed to convey datagrams will be pulled from this pool, instead of Xenomai's system pool. 
-         * 
+         * @param _pool_size local pool size: memory needed to convey datagrams will be pulled from this pool, instead of Xenomai's system pool.
+         *
          */
         XDDP_pipe ( int _pool_size = 8192 ) : pool_size ( _pool_size ) { fd = 0; }
 
         /**
          * @brief Getter for the XDDP pipe file descriptor.
-         * 
+         *
          * @return int XDDP pipe file descriptor
          */
         int get_fd() { return fd; }
 
         /**
          * @brief XDDP pipe initialization: xddp_bind for Xenomai system, mkfifo and open for standard linux kernel
-         * 
+         *
          * @param pipe_name name of the pipe : the pipe_prefix will be automatically preponed.
          * @return void
          */
@@ -86,15 +87,23 @@ namespace XBot{
             #ifdef __XENO__
                     fd = xddp_bind ( pipe_name.c_str(), pool_size );
             #else
-                    mkfifo ( pipe.c_str(), S_IRWXU|S_IRWXG );
-                    fd = open ( pipe.c_str(), O_RDWR | O_NONBLOCK );
+                    mq_attr attributes;
+                    attributes.mq_flags = O_NONBLOCK;
+                    attributes.mq_maxmsg = 1;
+                    attributes.mq_msgsize = 8192;
+                    attributes.mq_curmsgs = 0;
+
+                    fd = mq_open(("/"+pipe_name).c_str(), O_RDWR|O_CREAT|O_NONBLOCK, 0660, &attributes);
+                    if( fd < 0 ){
+                        perror("Unable to open message queue");
+                    }
             #endif
                     DPRINTF ( " .... open %s\n", pipe.c_str() );
         }
 
         /**
          * @brief XDDP pipe destructor: it will close the file descriptor linked to the pipe and in XENOMAI systems it will also unlik the pipe.
-         * 
+         *
          */
         virtual ~XDDP_pipe() {
             if ( ! fd ) {
@@ -108,25 +117,25 @@ namespace XBot{
     #endif
         }
 
-        
+
         /**
          * @brief template XDDP pipe non-blocking write function: it writes tx data on the fd linked to the pipe
-         * 
+         *
          * @param tx XddpTxTypes to write
          * @return int bytes wrote
          */
         template<typename XddpTxTypes>
         int xddp_write ( const XddpTxTypes & tx ) {
             if ( fd <= 0 ) { return 0; }
-            return write ( fd, ( void* ) &tx, sizeof ( tx ) );
+            return mq_send( fd, ( char* ) &tx, sizeof ( tx ), 1 );
         }
 
         /**
          * @brief simple XDDP pipe non-blocking write of a buffer of byte with size size
-         * 
+         *
          * @param buffer byte buffer to write
          * @param size buffer size
-         * 
+         *
          * @return int bytes wrote
          */
         int xddp_write ( const uint8_t * buffer, int size ) {
@@ -136,7 +145,7 @@ namespace XBot{
 
         /**
          * @brief template XDDP pipe non-blocking read function: it recvfrom (XENOMAI) or reads rx data on the fd linked to the pipe
-         * 
+         *
          * @param rx XddpRxTypes to read
          * @return int bytes read
          */
@@ -150,16 +159,17 @@ namespace XBot{
             return recvfrom ( fd, ( void* ) &rx, sizeof ( rx ), MSG_DONTWAIT, NULL, 0 );
     #else
             // NON-BLOCKING
-            return read ( fd, ( void* ) &rx, sizeof ( rx ) );
+            unsigned int prio;
+            return mq_receive(fd, (char *)&rx, sizeof(rx), &prio);
     #endif
         }
 
         /**
          * @brief simple XDDP pipe non-blocking read of a buffer of byte with size size
-         * 
+         *
          * @param buffer byte buffer to write
          * @param size buffer size
-         * 
+         *
          * @return int bytes wrote
          */
         int xddp_read ( const uint8_t * buffer, int size ) {
@@ -183,7 +193,7 @@ namespace XBot{
         int pool_size;
 
     };
-    
+
     // TBD define IDDP_pipe
 
 };
